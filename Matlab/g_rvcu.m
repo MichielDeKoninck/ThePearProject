aba4ffbf7bb721d55dbf7c8c9f3e9deec968b36e
Michiel De Koninck <dekoninckmichiel.mdk@gmail.com>
2019-04-06 21:37:28 +0200

    Peer with improved Jacobian
    Negative values in final Cu but structure looks very decent

--- Matlab/g_rvcu.m
+++ Matlab/g_rvcu.m
@@ -1,4 +1,4 @@
-function [g] = g_rvcu(phi_nr,node1,node2,node3,x1,x2,x3,ksi,eta,c_u,c_v,r_q,K_mu,K_mv,V_mu,K_mfu,V_mfv)
+function [g] = g_rvcu(Ak,phi_nr,node1,node2,node3,x1,x2,x3,ksi,eta,c_u,c_v,r_q,K_mu,K_mv,V_mu,K_mfu,V_mfv)
     phi_possibilities = [1-eta-ksi, ksi, eta];
     phi=phi_possibilities(phi_nr);
     c_u_coeff = c_u(node1)*(1-ksi-eta) + c_u(node2)*ksi + c_u(node3)*eta;
@@ -8,5 +8,6 @@ function [g] = g_rvcu(phi_nr,node1,node2,node3,x1,x2,x3,ksi,eta,c_u,c_v,r_q,K_mu
     dru_cu= (V_mu*K_mu)/((1+c_v_coeff/K_mv)*(K_mu+c_u_coeff)^2);
     
     r = (x1*(1-eta-ksi)+x2)*ksi+x3*eta;
-    g = -r*r_q*dru_cu*phi + r*drv_cu*phi;
+    
+    g = -r*r_q*dru_cu*phi*2*Ak + r*drv_cu*phi*2*Ak;
 end
